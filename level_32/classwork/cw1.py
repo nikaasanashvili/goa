b6a6f2a65e32feced4e27edbad497ebd84421839
nikaasanashvili <nikan6215@gmail.com>
2024-10-12 15:22:18 +0400

    goa

--- level_32/classwork/cw1.py
+++ level_32/classwork/cw1.py
@@ -1,4 +1,12 @@
 # შექმენით სია სადაც იქნება 10 სახელი შემდეგ ამ სიიდან ამოშლით ყველა იმ სახელს რომელიც იწყება ა ასოზე და დაბეჭდეთ გაფილტრული სია
 
 name=["atami", "vashli", "audi", "ananasi", "nika", "saba", "alucha", "msxali", "sazamtro", "anbani"]
+name1=[]
+
+for i in name:
+    if i [0] != "a":
+        name1.append(i)
+print(name1)
+
+
 
