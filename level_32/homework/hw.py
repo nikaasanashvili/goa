3246601d5f3eee60b8501bc842641c2591d0b451
nikaasanashvili <nikan6215@gmail.com>
2024-10-13 14:24:05 +0400

    goa

--- level_32/homework/hw.py
+++ level_32/homework/hw.py
@@ -3,8 +3,8 @@
 num=[12, 41, 14, 23, 18, 15, 51, 35, 19]
 larges=num[0]
 
-# for i in num:
-#     if i > larges:
-#         larges=i
-# print(larges)
+for i in num:
+    if i > larges:
+        larges=i
+print(larges)
 
