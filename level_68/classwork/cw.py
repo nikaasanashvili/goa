83cc4cfa0ce420c741ab6d950f6cfdabf817916f
nikaasanashvili <nikan6215@gmail.com>
2025-02-18 23:47:23 +0400

    goa

---
+++ level_68/classwork/cw.py
@@ -0,0 +1,9 @@
+def who_is_paying(name):
+    result=[]
+    if name==name[0:2]:
+        result.append(naem)
+    else:
+        result.append(name)
+        result.append(name[0:2])
+        
+    return result
